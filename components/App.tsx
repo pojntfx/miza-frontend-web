15eea7312e8e9e36a4976ec306d7f9af56eeba85
Felix Pojtinger <felix@pojtinger.com>
2020-05-15 13:04:37 +0200

    feat: Start implementation of toolbar

--- components/App.tsx
+++ components/App.tsx
@@ -4,8 +4,9 @@ import { Todo } from "../api/generated/api/todos_pb";
 import { ThemeProvider as StyledComponentsThemeProvider } from "styled-components";
 import { ThemeProvider as ThemeUIThemeProvider, Theme } from "theme-ui";
 import preset from "@rebass/preset";
-import { Heading, Text, Card, Box } from "rebass";
+import { Heading, Text, Card, Box, Button } from "rebass";
 import styled from "styled-components";
+import { FaPlus, FaRegCheckSquare, FaCog } from "react-icons/fa";
 
 const TodoCard = styled(Card)`
   padding: ${({ theme }: { theme: Theme }) => theme.space[1]}rem !important;
@@ -35,12 +36,53 @@ const Container = styled(Box)`
     (theme.space[8] as number) * 4}px;
   margin: 0 auto !important;
   padding: 0 ${({ theme }: { theme: Theme }) => theme.space[1]}rem;
+  margin-bottom: 4rem !important;
 `;
 
 const Header = styled.div`
   padding-top: ${({ theme }: { theme: Theme }) => theme.space[1]}rem !important;
   padding-bottom: ${({ theme }: { theme: Theme }) =>
     theme.space[1]}rem !important;
+`;
+
+const Toolbar = styled(Box)`
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  background: ${({ theme }: { theme: Theme }) => theme.colors.primary};
+  padding: ${({ theme }: { theme: Theme }) => theme.space[1]}rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  box-shadow: ${({ theme }: { theme: any }) => theme.shadows.card};
+`;
+
+const AddNoteButton = styled(Button)`
+  position: fixed;
+  bottom: ${({ theme }: { theme: Theme }) => theme.space[2]}rem;
+  left: 50%;
+  transform: translateX(-50%);
+  background: #ff8833 !important;
+  padding: ${({ theme }: { theme: Theme }) => (theme.space[1] as number) / 2}rem
+    ${({ theme }: { theme: Theme }) => theme.space[1]}rem !important;
+  border-radius: ${({ theme }: { theme: Theme }) =>
+    theme.space[1]}rem !important;
+  display: flex !important;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+
+  & > *:first-child {
+    margin-right: ${({ theme }: { theme: Theme }) =>
+      (theme.space[1] as number) / 2}rem;
+  }
+`;
+
+const IconButton = styled(Button)`
+  cursor: pointer;
+  display: flex !important;
+  justify-content: center;
+  align-items: center;
 `;
 
 const theme = {
@@ -75,6 +117,18 @@ export default () => {
             </TodoCard>
           ))}
         </Container>
+        <Toolbar>
+          <IconButton>
+            <FaRegCheckSquare />
+          </IconButton>
+          <AddNoteButton>
+            <FaPlus />
+            <span>Add Note</span>
+          </AddNoteButton>
+          <IconButton>
+            <FaCog />
+          </IconButton>
+        </Toolbar>
       </ThemeUIThemeProvider>
     </StyledComponentsThemeProvider>
   );

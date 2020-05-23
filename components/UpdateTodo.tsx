b3998cc80f704d62fddd68d32ba494c6652685f0
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-05-23 17:31:21 +0200

    feat: Add basic Google login

--- components/UpdateTodo.tsx
+++ components/UpdateTodo.tsx
@@ -3,13 +3,15 @@ import { TodosClient } from "../api/generated/api/todos_pb_service";
 import { Todo, TodoID } from "../api/generated/api/todos_pb";
 import { Box } from "rebass";
 import { Loading } from "./App";
+import { BrowserHeaders } from "browser-headers";
 
 interface IUpdateTodoProps {
   client: TodosClient;
   id: number;
+  token: string;
 }
 
-export default ({ client, id }: IUpdateTodoProps) => {
+export default ({ client, id, token }: IUpdateTodoProps) => {
   const [todo, setTodo] = React.useState<Todo>();
   const [loading, setLoading] = React.useState(false);
 
@@ -19,11 +21,17 @@ export default ({ client, id }: IUpdateTodoProps) => {
 
     setLoading(true);
 
-    client.get(todoID, (e, res) => {
-      e ? console.error(e) : setTodo(res);
+    client.get(
+      todoID,
+      new BrowserHeaders({
+        Authorization: `Bearer ${token}`,
+      }),
+      (e, res) => {
+        e ? console.error(e) : setTodo(res);
 
-      setLoading(false);
-    });
+        setLoading(false);
+      }
+    );
   }, []);
 
   return (

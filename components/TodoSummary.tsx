cd188822eba74d43737edb26f0686ac00edb3e9a
Felix Pojtinger <felix@pojtinger.com>
2020-05-28 18:46:56 +0200

    feat: Add optional checkbox for desktop select UX

--- components/TodoSummary.tsx
+++ components/TodoSummary.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
 import styled from "styled-components";
-import { Button, Box, Heading, Text } from "rebass";
+import { Box, Heading, Text } from "rebass";
 import { Theme } from "theme-ui";
-import { FaTimes } from "react-icons/fa";
+import { FaTimes, FaCheckSquare, FaRegCheckSquare } from "react-icons/fa";
+import { IconButton } from "./IconButton";
 
 interface ITodoSummaryProps {
   title: string;
@@ -10,15 +11,29 @@ interface ITodoSummaryProps {
   as?: any;
   to?: string;
   onClick: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  selectMode?: boolean;
+  onToggleSelect: (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => void;
+  selected?: boolean;
 }
 
 export const TodoSummary: React.FC<ITodoSummaryProps> = ({
   title,
   body,
   onClick,
+  selectMode,
+  onToggleSelect,
+  selected,
   ...rest
 }) => (
   <Wrapper {...rest}>
+    {selectMode && (
+      <TodoSelectButton onClick={onToggleSelect} inverted primary>
+        {selected ? <FaCheckSquare /> : <FaRegCheckSquare />}
+      </TodoSelectButton>
+    )}
+
     <Box overflowX="auto" mr={3}>
       {title != "" && <Heading>{title}</Heading>}
       {body != "" && <Text>{body}</Text>}
@@ -36,28 +51,12 @@ const Wrapper = styled(Box)<{ theme: Theme }>`
   justify-content: space-between;
   align-items: center;
   width: 100%;
+
+  & > *:nth-child(2):not(:last-child) {
+    flex: 1;
+  }
 `;
 
-const IconButton = styled(Button)<{ theme: Theme; inverted?: boolean }>`
-  min-width: initial !important;
-
-  cursor: pointer;
-  display: flex !important;
-  justify-content: center;
-  align-items: center;
-
-  transition-duration: 0.2s;
-  transition-timing-function: ease-in-out;
-  transition-delay: initial;
-  transition-property: all;
-  cursor: pointer;
-
-  color: ${({ theme, inverted }) =>
-    inverted ? `${theme.colors.text} !important` : "inherit"};
-  background: transparent !important;
-
-  &:hover,
-  &:active {
-    color: ${({ inverted }) => (inverted ? "red" : "#d9d9d9")} !important;
-  }
+const TodoSelectButton = styled(IconButton)<{ theme: Theme }>`
+  margin-right: ${({ theme }) => (theme.space[1] as number) / 2}rem !important;
 `;

981a2149622a23ed777697637fd7b903b7b0062d
Felix Pojtinger <felix@pojtinger.com>
2020-06-03 10:29:51 +0200

    feat: Add order forwards and backwards demo, return loading array if no todos have been received yet

--- neo/App.tsx
+++ neo/App.tsx
@@ -15,31 +15,45 @@ export const App: React.FC<IAppProps> = ({
   <LoginProvider googleClientId={googleClientId}>
     {({ token }) => (
       <DataProvider apiEndpoint={apiEndpoint} token={token} {...otherProps}>
-        {({ todos, createTodo, updateTodo, deleteTodo, reorderTodo }) => (
-          <>
-            <button onClick={() => createTodo("Test title", "Test body")}>
-              Create todo
-            </button>
-            <button
-              onClick={() =>
-                updateTodo(
-                  137,
-                  "Test title " + Date.now(),
-                  "Test body " + Date.now()
-                )
-              }
-            >
-              Update todo 137
-            </button>
-            <button onClick={() => deleteTodo(todos[todos.length - 1].id)}>
-              Delete last todo
-            </button>
-            <button onClick={() => reorderTodo(137, 1)}>
-              Reorder todo 137
-            </button>
-            {JSON.stringify(todos)}
-          </>
-        )}
+        {({
+          loading,
+          todos,
+          createTodo,
+          updateTodo,
+          deleteTodo,
+          reorderTodo,
+        }) =>
+          loading ? (
+            <>Loading ...</>
+          ) : (
+            <>
+              <button onClick={() => createTodo("Test title", "Test body")}>
+                Create todo
+              </button>
+              <button
+                onClick={() =>
+                  updateTodo(
+                    137,
+                    "Test title " + Date.now(),
+                    "Test body " + Date.now()
+                  )
+                }
+              >
+                Update todo 137
+              </button>
+              <button onClick={() => deleteTodo(todos[todos.length - 1].id)}>
+                Delete last todo
+              </button>
+              <button onClick={() => reorderTodo(137, 1)}>
+                Reorder todo 137 forwards
+              </button>
+              <button onClick={() => reorderTodo(137, -1)}>
+                Reorder todo 137 backwards
+              </button>
+              {JSON.stringify(todos.sort((a, b) => a.index - b.index))}
+            </>
+          )
+        }
       </DataProvider>
     )}
   </LoginProvider>

b730211b3201655b49223c016d4680f87a2a044b
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-06-08 21:01:59 +0200

    feat: Add `deleteTodo` action

--- neo-local-first/services/todos.ts
+++ neo-local-first/services/todos.ts
@@ -9,25 +9,34 @@ export interface IRemoteNewTodo {
 
 export interface IRemoteTodosService {
   create: (todo: IRemoteNewTodo) => IRemoteTodo;
+  delete: (todo: IRemoteTodo) => void;
 }
 
 export class RemoteTodosService implements IRemoteTodosService {
   private todos: IRemoteTodo[];
   private onCreated: (todo: IRemoteTodo) => void;
+  private onDeleted: (todo: IRemoteTodo) => void;
 
-  constructor(onCreated: (todo: IRemoteTodo) => void) {
+  constructor(
+    onCreated: (todo: IRemoteTodo) => void,
+    onDeleted: (todo: IRemoteTodo) => void
+  ) {
     this.todos = [];
     this.onCreated = onCreated;
+    this.onDeleted = onDeleted;
 
     setInterval(
       () =>
-        this.onCreated({
-          id: new Date().getTime(),
+        this.create({
           title: new Date().toLocaleDateString(),
           body: new Date().toLocaleTimeString(),
         }),
       1000
     );
+
+    setInterval(() => {
+      this.delete(this.todos[this.todos.length - 1]);
+    }, 2000);
   }
 
   create(todo: IRemoteNewTodo) {
@@ -38,5 +47,11 @@ export class RemoteTodosService implements IRemoteTodosService {
     setTimeout(() => this.onCreated(newTodo), 0);
 
     return newTodo;
+  }
+
+  delete(todo: IRemoteTodo) {
+    this.todos = this.todos.filter((t) => t.id != todo.id);
+
+    setTimeout(() => this.onDeleted(todo), 0);
   }
 }
--- neo-local-first/stores/todos.ts
+++ neo-local-first/stores/todos.ts
@@ -6,15 +6,20 @@ export interface ITodosStore {
   // Business logic
   todos: ILocalTodo[];
   createTodo: Action<ITodosStore, ILocalTodo>;
+  deleteTodo: Action<ITodosStore, ILocalTodo>;
 
   // Sync logic
   setRemoteTodoService: Action<ITodosStore, IRemoteTodosService>;
   handleRemoteTodoCreate: Action<ITodosStore, IRemoteTodo>;
+  handleRemoteTodoDelete: Action<ITodosStore, IRemoteTodo>;
 
   // Internal helpers
   idMappings: Map<ILocalTodo["id"], IRemoteTodo["id"]>;
   remoteTodosService: IRemoteTodosService;
+
+  // Remote sync listeners
   onCreateTodo: ActionOn<ITodosStore, ILocalTodo>;
+  onDeleteTodo: ActionOn<ITodosStore, ILocalTodo>;
 }
 
 export const todosStore = createStore<ITodosStore>({
@@ -25,6 +30,9 @@ export const todosStore = createStore<ITodosStore>({
 
     s.idMappings.set(p.id, p.id);
   }),
+  deleteTodo: action((s, p) => {
+    s.todos = s.todos.filter((t) => t.id != p.id);
+  }),
 
   // Sync logic
   setRemoteTodoService: action((s, p) => {
@@ -33,8 +41,8 @@ export const todosStore = createStore<ITodosStore>({
   handleRemoteTodoCreate: action((s, p) => {
     let foundMatchingId = false;
 
-    s.idMappings.forEach((mapping) => {
-      if (mapping.valueOf() == p.id) foundMatchingId = true;
+    s.idMappings.forEach((value) => {
+      if (value.valueOf() == p.id) foundMatchingId = true;
     });
 
     if (!foundMatchingId) {
@@ -48,16 +56,33 @@ export const todosStore = createStore<ITodosStore>({
       });
     }
   }),
+  handleRemoteTodoDelete: action((s, p) => {
+    let matchingId = "";
+
+    s.idMappings.forEach((value, key) => {
+      if (value.valueOf() == p.id) matchingId = key.toString();
+    });
+
+    if (matchingId) s.todos = s.todos.filter((t) => t.id != matchingId);
+  }),
 
   // Internal helpers
   idMappings: new Map(),
   remoteTodosService: null,
+
+  // Remote sync listeners
   onCreateTodo: actionOn(
     (actions) => actions.createTodo,
     (s, { payload }) => {
       const newTodo = s.remoteTodosService.create(payload);
 
       s.idMappings.set(payload.id, newTodo.id);
+    }
+  ),
+  onDeleteTodo: actionOn(
+    (actions) => actions.deleteTodo,
+    (s, { payload }) => {
+      s.remoteTodosService.delete(payload);
     }
   ),
 });

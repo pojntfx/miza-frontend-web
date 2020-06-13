532b294da9dbed93601783c09634bd012df18df8
Felix Pojtinger <felix@pojtinger.com>
2020-06-13 18:58:19 +0200

    feat: Start clean re-implementation of split approach

--- local-connector-remote-approach/services/local/todos.ts
+++ local-connector-remote-approach/services/local/todos.ts
@@ -1,15 +1,32 @@
-export interface ILocalNewTodo {
+import { injectable, inject } from "tsyringe";
+import { TodosServiceConnector } from "../connector/todos";
+import { v4 } from "uuid";
+
+export interface TodoLocalNew {
   title: string;
 }
 
-export interface ILocalTodo extends ILocalNewTodo {
+export interface TodoLocal extends TodoLocalNew {
   id: string;
 }
 
-export interface LocalTodosService {
-  create: (todo: ILocalNewTodo) => ILocalTodo;
+export interface TodosServiceLocal {
+  create(todo: TodoLocalNew): void;
 }
 
-export class LocalTodosServiceImpl implements LocalTodosService {
-    
+@injectable()
+export class TodosServiceLocalImpl implements TodosServiceLocal {
+  private todos: TodoLocal[] = [];
+
+  constructor(
+    @inject("TodosServiceConnector") private connector?: TodosServiceConnector
+  ) {}
+
+  create(todo: TodoLocalNew) {
+    const newTodo = { ...todo, id: v4() };
+
+    this.todos.push(newTodo);
+
+    this.connector.create(newTodo);
+  }
 }

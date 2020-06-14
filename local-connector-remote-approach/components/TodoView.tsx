f8c9f255cf06cb33828fd92a8d2179b7e21e1e04
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-06-14 13:08:32 +0200

    refactor: Decompose logic and state into data provider

--- local-connector-remote-approach/components/TodoView.tsx
+++ local-connector-remote-approach/components/TodoView.tsx
@@ -1,80 +1,57 @@
 import * as React from "react";
-import { TodosServiceLocal, TodoLocal } from "../services/local/todos";
-import { container } from "tsyringe";
+import { DataProvider } from "./DataProvider";
 
 export interface ITodoViewProps {}
 
-export const TodoView: React.FC<ITodoViewProps> = (props) => {
-  const [todos, setTodos] = React.useState<TodoLocal[]>([]);
-  const [todosService, setTodosService] = React.useState<TodosServiceLocal>();
+export const TodoView: React.FC<ITodoViewProps> = (props) => (
+  <DataProvider apiUrl="" token="">
+    {({ loading, todos, createTodo, deleteTodo, updateTodo, reorderTodo }) => (
+      <div {...props}>
+        {loading && "Loading ..."}
+        <button
+          onClick={() =>
+            createTodo({
+              title: `Title created from view at ${new Date().toLocaleString()}`,
+              body: `Body created from view at ${new Date().toLocaleString()}`,
+            })
+          }
+        >
+          Create todo
+        </button>
 
-  React.useEffect(() => {
-    const todosServiceLocal = container.resolve(
-      "TodosServiceLocal"
-    ) as TodosServiceLocal;
+        <button onClick={() => deleteTodo(todos[todos.length - 1].id)}>
+          Delete last todo
+        </button>
 
-    todosServiceLocal.on("created", async (todo) =>
-      setTodos((oldTodos) => [...oldTodos, todo])
-    );
+        <button
+          onClick={() =>
+            updateTodo({
+              ...todos[todos.length - 1],
+              title: `Title updated from view at ${new Date().toLocaleString()}`,
+            })
+          }
+        >
+          Update last todo
+        </button>
 
-    todosServiceLocal.on("deleted", async (id) =>
-      setTodos((oldTodos) => oldTodos.filter((todo) => todo.id != id))
-    );
+        <button onClick={() => reorderTodo(todos[0].id, -1)}>
+          Reorder first todo backwards
+        </button>
 
-    todosServiceLocal.on("updated", async (todo) =>
-      setTodos((oldTodos) =>
-        oldTodos.map((oldTodo) => (oldTodo.id == todo.id ? todo : oldTodo))
-      )
-    );
+        <button onClick={() => reorderTodo(todos[0].id, 1)}>
+          Reorder first todo forwards
+        </button>
 
-    setTodosService(todosServiceLocal);
-  }, []);
+        <div>Todo count: {todos.length}</div>
 
-  return (
-    <div {...props}>
-      <button
-        onClick={() =>
-          todosService.create({
-            title: `Title created from view at ${new Date().toLocaleString()}`,
-            body: `Body created from view at ${new Date().toLocaleString()}`,
-          })
-        }
-      >
-        Create todo
-      </button>
-
-      <button onClick={() => todosService.delete(todos[todos.length - 1].id)}>
-        Delete last todo
-      </button>
-
-      <button
-        onClick={() =>
-          todosService.update({
-            ...todos[todos.length - 1],
-            title: `Title updated from view at ${new Date().toLocaleString()}`,
-          })
-        }
-      >
-        Update last todo
-      </button>
-
-      <button onClick={() => todosService.reorder(todos[0].id, -1)}>
-        Reorder first todo backwards
-      </button>
-
-      <button onClick={() => todosService.reorder(todos[0].id, 1)}>
-        Reorder first todo forwards
-      </button>
-
-      <div>Todo count: {todos.length}</div>
-
-      <ul>
-        {todos
-          .sort((a, b) => a.index - b.index)
-          .map((todo) => (
-            <li key={todo.id}>{JSON.stringify(todo)}</li>
-          ))}
-      </ul>
-    </div>
-  );
-};
+        <ul>
+          {todos
+            .sort((a, b) => a.index - b.index)
+            .map((todo) => (
+              <li key={todo.id}>{JSON.stringify(todo)}</li>
+            ))}
+        </ul>
+      </div>
+    )}
+  </DataProvider>
+);
